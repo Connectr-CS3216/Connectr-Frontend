b57a095419a619f5be4c073a8f0630afa5d9cbbb
Nicholette <nekonekonik@gmail.com>
2016-08-17 14:22:47 +0800

    Initialise with angular-generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name connectrFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the connectrFrontendApp
+ */
+angular.module('connectrFrontendApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
