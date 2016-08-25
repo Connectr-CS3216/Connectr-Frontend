23ee3e23d03164372a928e3a3d8746ad84ae19b0
Jiang Sheng <gisonrg@gmail.com>
2016-08-25 15:47:30 +0800

    Fix the Facebook button rendering issue

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,11 +1,15 @@
 'use strict';
 
-angular.module('connectrFrontendApp').controller('HomeCtrl', function ($scope, $location) {
-   var height = $(window).height() + 5;
-   var unitHeight = parseInt(height) + 'px';
-   $('.homepage').css('height',unitHeight);
+angular.module('connectrFrontendApp').controller('HomeCtrl', function ($scope, $location, $window) {
+  var height = $(window).height() + 5;
+  var unitHeight = parseInt(height) + 'px';
+  $('.homepage').css('height',unitHeight);
 
     $scope.isActive = function (viewLocation) { 
         return viewLocation === $location.path();
     };
+  
+  if ($window.FB) {
+    FB.XFBML.parse(); // re-render the facebook login button
+  }
 });
