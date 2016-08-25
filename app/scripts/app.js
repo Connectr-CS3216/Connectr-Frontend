e27547aae9fa0dca0aabc284f432f7f71655e4e8
Nicholette <nekonekonik@gmail.com>
2016-08-26 00:15:50 +0800

    add smooth scrolling

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -60,7 +60,7 @@ angular
 
       (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
+        if (d.getElementById(id)) {return;}
         js = d.createElement(s); js.id = id;
         js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7&appId=631439630344992";
         fjs.parentNode.insertBefore(js, fjs);
--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('connectrFrontendApp').controller('HomeCtrl', function ($scope, $location, $window) {
+angular.module('connectrFrontendApp').controller('HomeCtrl', function ($scope, $location, $window, anchorSmoothScroll) {
   var height = $(window).height() + 5;
   var unitHeight = parseInt(height) + 'px';
   $('.homepage').css('height',unitHeight);
@@ -12,5 +12,11 @@ angular.module('connectrFrontendApp').controller('HomeCtrl', function ($scope, $
   if ($window.FB) {
     FB.XFBML.parse(); // re-render the facebook login button
   }
+
+  $scope.scrollTo = function(id) {
+    $location.hash(id);
+    anchorSmoothScroll.scrollTo(id);
+    $location.hash('');
+  };
 
 });
