6f1d87c73207568b17af9ca1d88e8b6597ffa670
Jinghan <phantom.there@gmail.com>
2016-08-25 22:36:37 +0800

    1. orderly visit my checkins 2. stop option during playing

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -19,6 +19,10 @@ angular.module('connectrFrontendApp').controller('MapCtrl', function ($scope, $l
         'collapse': 'panel-expanded'
     }
 
+    $scope.playButtonClassBinding = {
+        'play': 'glyphicon glyphicon-play' 
+    }
+
     $scope.toggleLeftPanel = function() {
         if ($scope.collapseClassBinding.collapse === 'panel-expanded') {
             $scope.collapseClassBinding.collapse = 'panel-collapse'
@@ -27,32 +31,49 @@ angular.module('connectrFrontendApp').controller('MapCtrl', function ($scope, $l
         }
     }
 
+    var previousCenter, previousZoom
     $scope.walkThrough = function() {
-        if (session.checkins) {
-            $scope.$broadcast("map.needsRemoveControl");
-            if ($scope.collapseClassBinding.collapse === 'panel-expanded') {
-                $scope.collapseClassBinding.collapse = 'panel-collapse'
-            }
-            playback(0)
+        if ($scope.playButtonClassBinding.play === "glyphicon glyphicon-stop") {
+            stopPlaying();
         } else {
-            alert("data not ready")
+            previousCenter = session.map.getCenter();
+            previousZoom = session.map.getZoom();
+
+            if (session.checkins) {
+                $scope.$broadcast("map.needsRemoveControl");
+                $scope.playButtonClassBinding.play = "glyphicon glyphicon-stop"
+                if ($scope.collapseClassBinding.collapse === 'panel-expanded') {
+                    $scope.collapseClassBinding.collapse = 'panel-collapse'
+                }
+                playback(0)
+            } else {
+                alert("data not ready")
+            }
         }
     }
 
-    var lastPlaceID
+    var lastPlaceID, timer
+    function stopPlaying() {
+        var map = session.map
+        map.flyTo({
+            center: previousCenter,
+            pitch: 0,
+            zoom: previousZoom
+        })
+
+        window.clearTimeout(timer)
+        map.once('moveend', function(){})
+        $scope.$broadcast("map.needsDisplayControl");
+        $scope.playButtonClassBinding.play = "glyphicon glyphicon-play"
+    }
+
     function playback(index) {
         var map = session.map
 
         var locations = session.checkins.features
 
         if (index === locations.length) {
-            map.flyTo({
-                center: [103.8198, 1.3521],
-                pitch: 0,
-                zoom: 9
-            })
-
-            $scope.$broadcast("map.needsRemoveControl");
+            stopPlaying()
             return
         }
 
@@ -66,13 +87,13 @@ angular.module('connectrFrontendApp').controller('MapCtrl', function ($scope, $l
         // Animate the map position based on camera properties
         map.flyTo({
             center: locations[index].geometry.coordinates,
-            pitch: 60,
+            pitch: 45,
             zoom: 16
         })
 
         map.once('moveend', function() {
             // Duration the slide is on screen after interaction
-            window.setTimeout(function() {
+            timer = window.setTimeout(function() {
                 // Increment index
                 playback(++index);
             }, 3000); // After callback, show the location for 3 seconds.
@@ -105,6 +126,8 @@ angular.module('connectrFrontendApp').controller('MapCtrl', function ($scope, $l
             'format': 'geojson'
         }) .success(function(data) {
             session.checkins = data;
+            session.checkins.features.sort(function (a, b) {return a.properties.checkin_time < b.properties.checkin_time ? -1 : 1})
+            console.log(data)
             // Notify map to read data
             $scope.$broadcast("api.selfCheckinsLoaded", data);
         }).error(function() {
