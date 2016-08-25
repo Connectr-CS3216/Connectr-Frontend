a878138db6aef862930cfa1bb6144dc56033fe00
Nicholette <nekonekonik@gmail.com>
2016-08-25 14:04:54 +0800

    add privacy policy

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,7 +1,11 @@
 'use strict';
 
-angular.module('connectrFrontendApp').controller('HomeCtrl', function () {
+angular.module('connectrFrontendApp').controller('HomeCtrl', function ($scope, $location) {
    var height = $(window).height() + 5;
    var unitHeight = parseInt(height) + 'px';
    $('.homepage').css('height',unitHeight);
+
+    $scope.isActive = function (viewLocation) { 
+        return viewLocation === $location.path();
+    };
 });
